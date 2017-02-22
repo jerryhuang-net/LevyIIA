6408c5255ca47b1e1058987a231833a0853edec6
jerryhuang-net <jerryhuang.net@hotmail.com>
2017-02-22 14:06:03 +0800

    support promotional discount and channel discount

--- GI.LevyIIA/LevyIIA.cs
+++ GI.LevyIIA/LevyIIA.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,9 +17,13 @@ namespace GI.LevyIIA
         /// </summary>
         public decimal LevyCap { get; set; }
         /// <summary>
-        /// the levy amount need to charge
+        /// the levy amount need to charge from customer
         /// </summary>
         public decimal LevyAmount { get; set; }
+        /// <summary>
+        /// the levy amount need to charge from sales channel
+        /// </summary>
+        public decimal? LevyAmountChannel { get; set; }
         enum LevyYear
         {
             /// <summary>
@@ -89,6 +93,37 @@ namespace GI.LevyIIA
                 Math.Round(levy.LevyRate * premium, 2)
                 );
             return levy;
+        }
+        /// <summary>
+        /// a promotional discount is a type of discount offers by the insurance company to client, the discount amount will not affect agent's commision
+        /// </summary>
+        /// <param name="effective"></param>
+        /// <param name="premium"></param>
+        /// <param name="discount"></param>
+        /// <returns></returns>
+        public static LevyIIA CalculateWithPromoDiscount(DateTime effective, decimal premium, decimal discount)
+        {
+            return Calculate(effective, premium - discount);
+        }
+        /// <summary>
+        /// a channel (aka sales channel) is normally a business unit of the insurance company, such as a direct sale marketing department.
+        /// A channel discount is a type of discount offers by the sales business unit, and so the levy is supposed to be splited into 2 parts: paid by client and paid by channel
+        /// </summary>
+        /// <param name="effective"></param>
+        /// <param name="premium"></param>
+        /// <param name="discount"></param>
+        /// <returns></returns>
+        public static LevyIIA CalculateWithChannelDiscount(DateTime effective, decimal premium, decimal discount)
+        {
+            var levyNoDiscount = Calculate(effective, premium);
+            var levyClient = Calculate(effective, premium - discount);
+            var levyChannel= Calculate(effective,  discount);
+            levyClient.LevyAmountChannel = levyChannel.LevyAmount;
+            if (levyNoDiscount.LevyAmount!= levyClient.LevyAmount+ levyChannel.LevyAmount)
+            {//[if levy reached cap, levy borne by channel = cap – levy paid by client]
+                levyClient.LevyAmountChannel = levyNoDiscount.LevyCap - levyClient.LevyAmount;
+            }
+            return levyClient;
         }
     }
 }
